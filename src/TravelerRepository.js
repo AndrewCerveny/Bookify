b4f4e21a918db90ebb0ffc2038b6347f8f7a970e
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2023-01-11 11:37:44 -0500

    finished TravelerRepo class and Test

---
+++ src/TravelerRepository.js
@@ -0,0 +1,10 @@
+class TravelerRepository {
+    constructor(travelDataSet) {
+        this.allTravelData = travelDataSet    
+    }
+    findById(id) {
+    const match = this.allTravelData.find(person => person.id === id)
+    return match
+    }
+}
+export default TravelerRepository
