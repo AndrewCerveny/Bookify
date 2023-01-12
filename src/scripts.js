e608f5d666f9ac031b04e7bff40639965c8b0faa
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2023-01-12 11:44:09 -0500

    Connected my fetch calls and created my instances

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,80 @@
-// This is the JavaScript entry file - your code begins here
+
 // Do not delete or rename this file ********
-
+// Imports Below 
 // An example of how you tell webpack to use a CSS (SCSS) file
+//  Imported Images
 import './css/styles.css';
-
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+// imported Files
+
+import Destination from './Destination';
+import DestinationRepository from './DestinationRepository';
+import Traveler from './Traveler';
+import TravelerRepository from './TravelerRepository';
+import Trips from './Trips';
+import TripsRepository from './TripsRepository';
+import Trip from './Trips';
+
+// global Variables
+let allDestinations;
+let destinationRepo;
+let allTravelers;
+let travelerRepo;
+let allTrips;
+let tripsRepo;
+let yearAgo; 
+let currentUser;
+
+// global Const Variables
+const TodayDate = "2022/10/06";
+const travelerUrl = 'http://localhost:3001/api/v1/travelers';
+const tripsUrl = 'http://localhost:3001/api/v1/trips';
+const destinationsUrl = 'http://localhost:3001/api/v1/destinations';
+
+//  Api Fetch Area 
+function snagApiData(url) {
+    return fetch(url)
+				.then((response) => {
+			if(!response.ok) {
+				throw new Error('Whoops')
+			}else {
+			return response.json()
+			}
+		})
+		.catch((err) => console.log(err.message))
+};
+
+function gatherDatasets() {
+	Promise.all([
+		snagApiData(travelerUrl),
+		snagApiData(tripsUrl),
+		snagApiData(destinationsUrl)
+	])
+	.then((data) => {
+		allTravelers = data[0].travelers
+		allTrips =  data[1].trips
+		allDestinations = data[2].destinations
+		createInstances(allTravelers, allTrips,allDestinations)
+	})
+}
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+// Event Listeners
+window.addEventListener('load', gatherDatasets)
+
+
+
+
+
+
+// function area
+function createInstances(dataSet1, dataSet2, dataSet3) {
+	allTravelers = dataSet1.map(person => new Traveler(person));
+	travelerRepo = new TravelerRepository(allTravelers);
+	allTrips = dataSet2.map(trip => new Trip(trip));
+	tripsRepo = new TripsRepository(allTrips);
+	allDestinations = dataSet3.map(place => new Destination(place));
+	destinationRepo = new DestinationRepository(allDestinations);
+	console.log('Boot',destinationRepo)
+}
